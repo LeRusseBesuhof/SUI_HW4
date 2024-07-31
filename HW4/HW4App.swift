164a64254fa111f3868167c882c87057416e0c76
LeRusseBesuhof <p.grad0ff@gmail.com>
2024-07-31 20:42:06 +0700

    Initial Commit

---
+++ HW4/HW4App.swift
@@ -0,0 +1,17 @@
+//
+//  HW4App.swift
+//  HW4
+//
+//  Created by Павел Градов on 31.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct HW4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
